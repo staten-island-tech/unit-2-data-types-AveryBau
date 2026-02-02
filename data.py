a1fda94811ec0d865f6d2216c8d1d0cadd1da162
AveryBau <averya34@nycstudents.net>
2026-02-02 10:17:11 -0500

    Day 5

---
+++ data.py
@@ -0,0 +1,34 @@
+""" import turtle
+from turtle import *
+t = Turtle()
+ """
+""" x = 3 
+y = float(3)
+print(x,y) """
+
+""" values = [1,2,23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+day_of_week = input("what day is it?")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
+
+sentence = input("please enter a sentence")
+if sentence == input:
+    print("correct")
+    
+
+
+
+x = "test"
+print(f"hello {x}")
